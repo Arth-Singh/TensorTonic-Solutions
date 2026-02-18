85f5629f06d66a4964979147ad754629e0007839
Arth Singh <codingarth@gmail.com>
2026-02-19 06:24:13 +0900

    Update solution for cosine-embedding-loss

--- cosine-embedding-loss/cosine-embedding-loss.py
+++ cosine-embedding-loss/cosine-embedding-loss.py
@@ -2,26 +2,26 @@ import numpy as np
 
 def cosine_embedding_loss(x1, x2, label, margin = 0.0):
 
-  x1 = np.array(x1, dtype = np.float64)
-  x2 = np.array(x2, dtype = np.float64)
-
-  dot_product = np.dot(x1, x2)
-
-  norm_x1 = np.linalg.norm(x1)
-
-  norm_x2 = np.linalg.norm(x2)
-
-  cosine_sim = dot_product/(norm_x1*norm_x2)
+    x1 = np.array(x1, dtype = np.float64)
+    x2 = np.array(x2, dtype = np.float64)
 
 
-#Loss 
+    dot_product = np.dot(x1, x2)
 
-  if label == 1:
-    loss = 1.0 - cosine_sim
+    norm_x1 = np.linalg.norm(x1)
+    norm_x2 = np.linalg.norm(x2)  # linalg computed L2 norm 
 
-  else: 
-    loss = max(0.0, cosine_sim - margin)
+    #cosing loss
 
-  return float(loss)
+    cosine_sim = dot_product / (norm_x1 * norm_x2)
 
-  
+
+    if label == 1:
+        loss = 1.00 - cosine_sim
+
+    else:
+
+        loss = max(0.0, cosine_sim - margin)
+
+    return float(loss)
+    
